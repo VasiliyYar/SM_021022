2c0fa196615b812aafcf71a26844f785857ead41
Vasiliy Solovev <vasya.79@bk.ru>
2022-10-09 22:36:33 +0300

    Add branch stat

--- src/SalesManager.java
+++ src/SalesManager.java
@@ -14,4 +14,22 @@ public class SalesManager {
         }
         return max;
     }
+
+    public int average() {
+        int average = 0;
+        int max = 0;
+        int min = 0;
+
+        for (int sale : sales) {
+
+            if (sale > max) {
+                max = sale;
+            } else if (sale < max || sale < min) {
+                min = sale;
+            }
+            average = average + sale;
+        }
+        average = (average - max - min) / (sales.length - 2);
+        return average;
+    }
 }
