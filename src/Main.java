66f191b466fec465083b3734d6b74cc1848f9b40
Vasiliy Solovev <vasya.79@bk.ru>
2022-10-09 21:53:17 +0300

    Add branch longer

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int [] sale = new int[] {250, 600, 150, 40};
+        long [] sale = new long[] {250, 600, 150, 40};
         SalesManager salesManager = new SalesManager(sale);
-        int max = salesManager.max();
+        long max = salesManager.max();
 
         System.out.println(max);
 
